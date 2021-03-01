1abec7c3d45bf7c3aadbea76c614ab8fd8154c14
Marvin Rudolph <rudolph.marvin@web.de>
2021-03-01 20:29:48 +0100

    Initial Commit

---
+++ Stepper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stepper
+//
+//  Created by Marvin Rudolph on 01.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stepper/StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperApp.swift
+//  Stepper
+//
+//  Created by Marvin Rudolph on 01.03.21.
+//
+
+import SwiftUI
+
+@main
+struct StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
